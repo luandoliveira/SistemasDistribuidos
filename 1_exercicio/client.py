7a9aa73e21b2037af9456ce45e26f765373d4969
Luan de Oliveira <49959876+luandoliveira@users.noreply.github.com>
2021-09-14 16:10:22 -0400

    Update client.py

--- 1_exercicio/client.py
+++ 1_exercicio/client.py
@@ -5,31 +5,31 @@ server = '192.168.116.1'
 port = 8080
 encoding_format = 'utf-8'
 
-#Create socket
+#Criando um socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server,port))
 
-#Send the name of the author of the message.
+#Enviando nome do autor
 def send_name():
     name = str(input("Say your name: "))
     to_send("name=" + name)
 
-#Send the author's message
+#Mensagem do autor
 def send_message():
     while True:
         message = str(input())
         to_send("msg=" + message)
 
-#Send message
+#Enviar
 def to_send(message):
     client.send(message.encode(encoding_format))
 
-#Start sending name and message
+
 def start_send():
     send_name()
     send_message()
 
-#Handle_mensagens and displayed
+
 def handle_mensagens():
     while(True):
         msg = client.recv(1024).decode()
